ccec15cf6359cddfb83008dc3c82d07d49f9d464
HELOÍSA SILVEIRA LACCOTIS <heloisa.laccotis@portalsesisp.org.br>
2026-02-18 11:15:24 -0300

    add banner

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,45 @@
-import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import { Dimensions, FlatList, Image, ScrollView, StyleSheet, Text, Touchable, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+const bannerData = [
+  {
+    id: 1,
+    image: "https://thumbnails.cbsig.net/_x/w1200/CBS_Production_Entertainment_VMS/2022/10/26/2091444291941/TGMAV_SAlone_16_9_1920x1080_1781067_1920x1080.jpg",
+    title: "Top gun Maverick"
+  },
+  {
+    id: 2,
+    image: "https://upload.wikimedia.org/wikipedia/pt/e/e0/The_Vampire_Diaries_season_8_poster.jpg",
+    title: "The Vampire Diares"
+  },
+  {
+    id: 3,
+    image: "https://occ-0-8407-2218.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABRVrTjyoNJtfmlHtnQUZGZ8bHWGqj-70tfh4rTWxObUkIBfRqji61fSoUpkhmGHOFAHOpnyA0MT93oekkogHN3FE2XYys3xQN2nQ.jpg?r=47e",
+    title: "Bridgerton"
+  }
+]
 
 
 const filmesEmAlta = [
   {
     id: 1,
-    title: "27 dresses", 
-    image: "https://upload.wikimedia.org/wikipedia/pt/4/48/Twenty_seven_dresses.jpg" 
+    title: "27 dresses",
+    image: "https://upload.wikimedia.org/wikipedia/pt/4/48/Twenty_seven_dresses.jpg"
   },
-   {
+  {
     id: 2,
-    title: "A empregada", 
-    image: "https://ingresso-a.akamaihd.net/prd/img/movie/a-empregada/4e37e5ca-5b88-4eee-8bee-07842a605e2f.webp" 
-   },
+    title: "A empregada",
+    image: "https://ingresso-a.akamaihd.net/prd/img/movie/a-empregada/4e37e5ca-5b88-4eee-8bee-07842a605e2f.webp"
+  },
   {
     id: 3,
-    title: "O agente secreto", 
-    image: "https://br.web.img2.acsta.net/img/04/71/04712910ab37ea0722ef92320cf4e1ca.jpg" 
-   },
+    title: "O agente secreto",
+    image: "https://br.web.img2.acsta.net/img/04/71/04712910ab37ea0722ef92320cf4e1ca.jpg"
+  },
   {
     id: 4,
-    title: "Como eu era antes de você", 
-    image: "https://play-lh.googleusercontent.com/JQKfopRtWqyD1cUXjx4V00qLnKecgKlSTS6sL0YX1T0H4bzyOK8SxdQIUbZ6NTsKfkBm" 
+    title: "Como eu era antes de você",
+    image: "https://play-lh.googleusercontent.com/JQKfopRtWqyD1cUXjx4V00qLnKecgKlSTS6sL0YX1T0H4bzyOK8SxdQIUbZ6NTsKfkBm"
   }
 ]
 
@@ -38,8 +56,8 @@ const filme = [
   },
   {
     id: 3,
-    title: "My lady Jane", 
-    image:"https://m.media-amazon.com/images/M/MV5BNmEwYjQ5MWMtZDdiOC00NTk1LWEwOWEtYjQ2NDUxNjk2YWU5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
+    title: "My lady Jane",
+    image: "https://m.media-amazon.com/images/M/MV5BNmEwYjQ5MWMtZDdiOC00NTk1LWEwOWEtYjQ2NDUxNjk2YWU5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
   },
   {
     id: 4,
@@ -52,12 +70,12 @@ const favoritos = [
   {
     id: 1,
     title: "Minha vida em marte",
-    image:"https://s2-globo-play.glbimg.com/rVPkM8Zwde1MrBXL0tEp5ATs5Ps=/362x536/https://s2-globo-play.glbimg.com/sGvQR0uUFmxkmUFZH14_-Km8OGU=/https://s2.glbimg.com/Rjdsdee8bhYyDmITSlIEKQHMaSM=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/0/R/Uw3FeRQImS3hXsxR2Jwg/2020-927-minha-vida-em-marte-poster.jpg"
+    image: "https://s2-globo-play.glbimg.com/rVPkM8Zwde1MrBXL0tEp5ATs5Ps=/362x536/https://s2-globo-play.glbimg.com/sGvQR0uUFmxkmUFZH14_-Km8OGU=/https://s2.glbimg.com/Rjdsdee8bhYyDmITSlIEKQHMaSM=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/0/R/Uw3FeRQImS3hXsxR2Jwg/2020-927-minha-vida-em-marte-poster.jpg"
   },
   {
-    id: 2, 
-    title:"Crepuscúlo: Amanhecer part 2",
-    image:"https://cinema10.com.br/upload/filmes/filmes_1135_A-Saga-Crepusculo-Amanhecer-Parte-2-Poster.jpg"
+    id: 2,
+    title: "Crepuscúlo: Amanhecer part 2",
+    image: "https://cinema10.com.br/upload/filmes/filmes_1135_A-Saga-Crepusculo-Amanhecer-Parte-2-Poster.jpg"
   },
   {
     id: 3,
@@ -67,125 +85,193 @@ const favoritos = [
   },
   {
     id: 4,
-    title:"Nosso sonho",
+    title: "Nosso sonho",
     image: "https://upload.wikimedia.org/wikipedia/pt/2/28/2453404.jpg-c_310_420_x-f_jpg-q_x-xxyxx.jpg"
   }
 ]
 
+const { width } = Dimensions.get("window")
 // ---------------------------------------------------------
 export default function Index() {
   return (
     <SafeAreaView style={style.container}>
-      <View >
-      <Text style ={style.mainTitle}>Senaflix</Text>
-      <Text style={style.welcomeText}>O melhor app para quem ama filmes e series</Text>
-    </View>
 
-    <View style={style.section}>
-      <text style={style.sectionTitle}>Em alta</text>
+      <ScrollView>
+        <View >
+          <Text style={style.mainTitle}>Senaflix</Text>
+          <Text style={style.welcomeText}>O melhor app para quem ama filmes e series</Text>
+        </View>
 
-      <FlatList 
-      contentContainerStyle={style.movieList}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      data={filmesEmAlta} 
-      renderItem={(({item}) => (
-        <View style={style.movieItem}>
-          <Image style={style.movieImage} source={{uri: item.image }} />
-          <Text style={style.movieTitle}>{item.title}</Text>
-      </View>
-    ))} >
+        <View style={style.section}>
+          <FlatList
+            data={bannerData}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            renderItem={(({ item }) => (
+              <View>
+                <View style={style.bannerContainer}>
+                  <Image style={style.bannerImage} source={{ uri: item.image }}></Image>
+                  <Text style={style.bannerTitle}>{item.title}</Text>
 
-
-    </FlatList>
-    </View>
-    <View style={style.section}>
-      <text style={style.sectionTitle}>Series</text>
-
-      <FlatList 
-      contentContainerStyle={style.movieList}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      data={filme} 
-      renderItem={(({item}) => (
-        <View style={style.movieItem}>
-          <Image style={style.movieImage} source={{uri: item.image }} />
-          <Text style={style.movieTitle}>{item.title}</Text>
-      </View>
-    ))} >
-
-{/* --------------------------------------------------------------- */}
-    </FlatList>
-    </View>
-    <View style={style.section}>
-      <text style={style.sectionTitle}>Em alta</text>
-
-      <FlatList 
-      contentContainerStyle={style.movieList}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      data={favoritos} 
-      renderItem={(({item}) => (
-        <View style={style.movieItem}>
-          <Image style={style.movieImage} source={{uri: item.image }} />
-          <Text style={style.movieTitle}>{item.title}</Text>
-      </View>
-    ))} >
+                  <TouchableOpacity style={style.bannerButton}>
+                    <Text style={style.bannerButtonText}>Assistir</Text>
+                  </TouchableOpacity>
 
 
-    </FlatList>
-    </View>
+                </View>
+              </View>
+
+            ))}
+
+          />
+        </View>
+
+        <View style={style.section}>
+          <text style={style.sectionTitle}>Em alta</text>
+
+          <FlatList
+            contentContainerStyle={style.movieList}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={filmesEmAlta}
+            renderItem={(({ item }) => (
+            
+                <View style={style.movieItem}>
+                  <Image style={style.movieImage} source={{ uri: item.image }} />
+                  <Text style={style.movieTitle}>{item.title}</Text>
+                </View>
+             
+
+            ))} >
+
+
+          </FlatList>
+        </View>
+        <View style={style.section}>
+          <text style={style.sectionTitle}>Series</text>
+
+          <FlatList
+            contentContainerStyle={style.movieList}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={filme}
+            renderItem={(({ item }) => (
+              <View style={style.movieItem}>
+                <Image style={style.movieImage} source={{ uri: item.image }} />
+                <Text style={style.movieTitle}>{item.title}</Text>
+              </View>
+            ))} >
+
+            {/* --------------------------------------------------------------- */}
+          </FlatList>
+        </View>
+        <View style={style.section}>
+          <text style={style.sectionTitle}>Em alta</text>
+
+          <FlatList
+            contentContainerStyle={style.movieList}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={favoritos}
+            renderItem={(({ item }) => (
+              <View style={style.movieItem}>
+                <Image style={style.movieImage} source={{ uri: item.image }} />
+                <Text style={style.movieTitle}>{item.title}</Text>
+              </View>
+            ))} >
+
+
+          </FlatList>
+        </View>
+      </ScrollView>
     </SafeAreaView>
-    
+
   );
 }
 // --------------------------------------------------------------
 const style = StyleSheet.create({
   container: {
     paddingTop: 10,
-    paddingHorizontal: 15, 
-    backgroundColor: "#2B0F0F" ,
+    paddingHorizontal: 15,
+    backgroundColor: "#2B0F0F",
     flex: 1,
   },
   mainTitle: {
-    color:"#fff",
+    color: "#fff",
     fontSize: 20,
-    fontWeight: "bold" ,
+    fontWeight: "bold",
     marginBottom: 5
   },
   welcomeText: {
-    color:"#fff",
+    color: "#fff",
     fontSize: 14,
     opacity: 0.8,
     marginBottom: 25
   },
   movieImage: {
-    width: 120, 
-    height: 180, 
+    width: 120,
+    height: 180,
     borderRadius: 8
   },
   movieTitle: {
     color: "#c6c6c6",
     marginTop: 5,
-    fontSize: 12, 
-    fontWeight:600,
+    fontSize: 12,
+    fontWeight: 600,
     flexWrap: "wrap"
   },
 
-movieItem: {
-  width: 120 
-},
+  movieItem: {
+    width: 120
+  },
 
   sectionTitle: {
-    color:"#e5e5e5",
+    color: "#e5e5e5",
     fontSize: 14,
     fontWeight: "bold",
-    marginBottom: 10 
+    marginBottom: 10
   },
   movieList: {
     gap: 15
   },
   section: {
     marginBottom: 30
-   }
+  },
+  bannerImage: {
+    width: "100%",
+    height: 200,
+    resizeMode: "cover",
+    borderRadius: 8
+  },
+  bannerTitle: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+    position: "absolute",
+    bottom: 50,
+    left: 15
+  },
+  bannerItem: {
+    flex: 1,
+    height: 200,
+    paddingHorizontal: 5,
+    position: "relative"
+  },
+  bannerContainer: {
+    width: width - 30
+  },
+  bannerButton: {
+    backgroundColor: "rgb(3, 40, 119)",
+    borderRadius: 5,
+    paddingVertical: 6,
+    paddingHorizontal: 16,
+    position: "absolute",
+    bottom: 10,
+    left: 15
+  },
+  bannerButtonText: {
+    color:"#fff",
+    fontSize: 13,
+    fontWeight:"bold"
+  }
 })
